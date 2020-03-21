0a9fda09772a5f147c3e81008ea02a35c487f001
akshaymittur <akshaymittur@gmail.com>
2020-03-21 13:31:25 +0530

    Switched from ant to semantic

---
+++ src/components/HomePage/HomeNav.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Menu, Button, Icon, Image } from "antd";
+import { Link } from "react-router-dom";
+
+function HomeNav() {
+  return (
+    <Menu>
+      <Image
+        src="https://img.icons8.com/office/40/000000/class.png"
+        size="mini"
+        style={{
+          height: "40px",
+          width: "40px",
+          marginLeft: "0.5%",
+          paddingTop: "0.5%"
+        }}
+      />
+      <span
+        className="txt"
+        style={{ color: "#008080", paddingTop: "1%", marginLeft: "0.5%" }}
+      >
+        CoDash
+      </span>
+      <Menu.Menu position="right">
+        <Menu.Item>
+          <Button size="large" as={Link} to="/signup" animated="fade">
+            <Button.Content visible>Sign Up</Button.Content>
+            <Button.Content hidden>Join Us!</Button.Content>
+          </Button>
+        </Menu.Item>
+
+        <Menu.Item>
+          <Button size="large" color="teal" as={Link} to="/login" animated>
+            <Button.Content visible>Login</Button.Content>
+            <Button.Content hidden>
+              <Icon name="arrow right" />
+            </Button.Content>
+          </Button>
+        </Menu.Item>
+      </Menu.Menu>
+    </Menu>
+  );
+}
+
+export default HomeNav;
---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeNav from "./HomeNav";
+
+function HomePage() {
+  return (
+    <div>
+      <HomeNav />
+    </div>
+  );
+}
+
+export default HomePage;
