5b0b8dee9b70094556dfda3e2f506fc3c0f0aa50
akshaymittur <akshaymittur@gmail.com>
2020-04-03 22:12:23 +0530

    dsad

--- src/components/AnnotatorDashboard/AnnotatorDashboard.js
+++ src/components/AnnotatorDashboard/AnnotatorDashboard.js
@@ -14,6 +14,7 @@ import {
 } from "semantic-ui-react";
 import { Link, withRouter } from "react-router-dom";
 import axios from "axios";
+import ImageAnnotation from "./ImageAnnotation";
 
 const ENDPOINTLOGOUT =
   "https://image-annotation-backend.herokuapp.com/annotator/logout";
@@ -67,6 +68,7 @@ function AnnotatorDashboard(props) {
           </Dropdown>
         </Menu.Menu>
       </Menu>
+      <ImageAnnotation />
     </div>
   );
 }
