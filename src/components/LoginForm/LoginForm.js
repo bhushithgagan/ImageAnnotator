f0645279e124dbc5e1e71a0ba36c9e405491d377
akshaymittur <akshaymittur@gmail.com>
2020-04-07 21:29:58 +0530

    Login Integration done but CORS problem present

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -6,7 +6,8 @@ import {
   Grid,
   Segment,
   Header,
-  Message
+  Message,
+  Tab,
 } from "semantic-ui-react";
 import { Link, withRouter } from "react-router-dom";
 import axios from "axios";
@@ -21,13 +22,117 @@ function LoginForm(props) {
   const [errors, setErrors] = useState({});
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [type, setType] = useState(true); //true = user false = annotator
+  const panes = [
+    {
+      menuItem: "User",
+      render: () => (
+        <Tab.Pane attached={false}>
+          <Form error size="large" onSubmit={handleSubmitUser}>
+            <Segment raised inverted color="teal" secondary className="zoomIn">
+              <Form.Input
+                fluid
+                onChange={handleUsernameChange}
+                icon="address card"
+                iconPosition="left"
+                placeholder="Username"
+                name="username"
+                type="input"
+                className="zoomIn"
+              />
+              <Form.Input
+                fluid
+                onChange={handlePasswordChange}
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                name="password"
+                type="password"
+                className="zoomIn"
+              />
+
+              <Button
+                type="submit"
+                className="zoomIn"
+                loading={load}
+                onClick={() => setLoad(true)}
+              >
+                Login
+              </Button>
+              <Button inverted as={Link} to="/signup" className="zoomIn">
+                Sign Up
+              </Button>
+            </Segment>
+            {Object.entries(errors).length > 0 && (
+              <Message
+                error
+                header="Could Not Sign In"
+                list={Object.keys(errors).map((key) => errors[key])}
+                size="small"
+                className="zoomIn"
+              />
+            )}
+          </Form>
+        </Tab.Pane>
+      ),
+    },
+    {
+      menuItem: "Annotator",
+      render: () => (
+        <Tab.Pane attached={false}>
+          <Form error size="large" onSubmit={handleSubmitAnnotator}>
+            <Segment raised inverted color="teal" secondary className="zoomIn">
+              <Form.Input
+                fluid
+                onChange={handleUsernameChange}
+                icon="address card"
+                iconPosition="left"
+                placeholder="Username"
+                name="username"
+                type="input"
+                className="zoomIn"
+              />
+              <Form.Input
+                fluid
+                onChange={handlePasswordChange}
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                name="password"
+                type="password"
+                className="zoomIn"
+              />
+
+              <Button
+                type="submit"
+                className="zoomIn"
+                loading={load}
+                onClick={() => setLoad(true)}
+              >
+                Login
+              </Button>
+              <Button inverted as={Link} to="/signup" className="zoomIn">
+                Sign Up
+              </Button>
+            </Segment>
+            {Object.entries(errors).length > 0 && (
+              <Message
+                error
+                header="Could Not Sign In"
+                list={Object.keys(errors).map((key) => errors[key])}
+                size="small"
+                className="zoomIn"
+              />
+            )}
+          </Form>
+        </Tab.Pane>
+      ),
+    },
+  ];
 
   document.title = "DaNotate | Login";
 
-  const handleUsernameChange = event => setUsername(event.target.value);
-  const handlePasswordChange = event => setPassword(event.target.value);
-  const handleTypeChange = event => setType(!type);
+  const handleUsernameChange = (event) => setUsername(event.target.value);
+  const handlePasswordChange = (event) => setPassword(event.target.value);
 
   //if (submitResponse === true) props.history.push("/dashboard");
 
@@ -35,15 +140,61 @@ function LoginForm(props) {
     if (Object.entries(errors).length > 0) setLoad(false);
   }, [errors]);
 
-  function handleSubmit(data) {
+  function handleSubmitUser(data) {
+    let error = {};
+    if (!password || !username) {
+      error.fill = "Make sure you fill in all the fields";
+      setLoad(false);
+    }
+
+    setErrors(error);
+
+    if (!Object.entries(error).length > 0)
+      axios
+        .get(
+          ENDPOINTUSER,
+          {},
+          {
+            auth: {
+              username,
+              password,
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((error) => {
+          console.log(error);
+          setLoad(false);
+        });
+  }
+
+  function handleSubmitAnnotator(data) {
     let error = {};
     if (!password || !username)
       error.fill = "Make sure you fill in all the fields";
 
     setErrors(error);
 
-    if (type) axios.post(ENDPOINTUSER);
-    else axios.post(ENDPOINTANN);
+    if (!Object.entries(error).length > 0)
+      axios
+        .get(
+          ENDPOINTANN,
+          {},
+          {
+            auth: {
+              username: data.username,
+              password: data.password,
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
   }
 
   return (
@@ -57,61 +208,7 @@ function LoginForm(props) {
         >
           Hello, there. Login to your account
         </Header>
-        <Form error size="large" onSubmit={handleSubmit}>
-          <Segment raised inverted color="teal" secondary className="zoomIn">
-            <Form.Input
-              fluid
-              onChange={handleUsernameChange}
-              icon="address card"
-              iconPosition="left"
-              placeholder="Email"
-              name="email"
-              type="input"
-              className="zoomIn"
-            />
-            <Form.Input
-              fluid
-              onChange={handlePasswordChange}
-              icon="lock"
-              iconPosition="left"
-              placeholder="Password"
-              name="password"
-              type="password"
-              className="zoomIn"
-            />
-            <Form.Group inline>
-              <label>Account Type</label>
-              User
-              <Form.Radio
-                toggle
-                style={{ marginLeft: "20%", marginTop: "10%" }}
-                onChange={handleTypeChange}
-              />
-              Annotator
-            </Form.Group>
-
-            <Button
-              type="submit"
-              className="zoomIn"
-              loading={load}
-              onClick={() => setLoad(true)}
-            >
-              Login
-            </Button>
-            <Button inverted as={Link} to="/signup" className="zoomIn">
-              Sign Up
-            </Button>
-          </Segment>
-          {Object.entries(errors).length > 0 && (
-            <Message
-              error
-              header="Could Not Sign In"
-              list={Object.keys(errors).map(key => errors[key])}
-              size="small"
-              className="zoomIn"
-            />
-          )}
-        </Form>
+        <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
       </Grid.Column>
     </Grid>
   );
