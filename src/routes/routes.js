ac605f17722ef63c02bbf62768758389c6264631
akshaymittur <akshaymittur@gmail.com>
2020-04-10 23:22:39 +0530

    test1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,4 +10,4 @@ export const USERLOGOUT =
   "https://image-annotation-backend.herokuapp.com/user/logout";
 export const USERUPLOAD =
   "https://image-annotation-backend.herokuapp.com/user/images/upload";
-  export const lol = " ";
+  export const lol = "fdsf ";
