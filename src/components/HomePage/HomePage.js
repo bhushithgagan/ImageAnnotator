8fac19d7db108187ea6a20509ad20dd092b77a79
N B  Venkatesh <bhushith.cs18bmsce.ac.in>
2020-04-26 20:13:30 +0530

    Added title text animation

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import HomeNav from "./HomeNav";
 import HomeBody from "./HomeBody";
+import AnimatedText from "./AnimatedText";
+
 
 function HomePage() {
   return (
     <div>
       <HomeNav />
+      <AnimatedText />
       {/* <HomeBody /> */}
     </div>
   );
